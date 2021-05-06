296b7dc719447cbdda37817e17fb85cc917b2cd1
Dzenasad <no@more>
2021-05-06 23:59:32 +0300

    He we go again ><

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/model/alarm/MyAlarmManager.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/model/alarm/MyAlarmManager.kt
@@ -1,51 +1,49 @@
 package io.techmeskills.an02onl_plannerapp.model.alarm
 
-import android.R
-import android.app.AlarmManager
-import android.app.Notification
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
-import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import io.techmeskills.an02onl_plannerapp.R
+import android.app.NotificationManager
+import androidx.core.content.ContextCompat.getSystemService
 
 
 class MyAlarmManager {
 
-    private var pendingIntent: PendingIntent? = null
 
     fun setAlarm(context: Context, alarmTime: Long, message: String) {
-        val alarmManager: AlarmManager =
-            context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        lateinit var mChannel: NotificationChannel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mChannel = NotificationChannel(CHANNEL_ID, MY_APP_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            nm.createNotificationChannel(mChannel)
+        }
 
-        val intent = Intent(context, MyIntentService::class.java)
-        intent.action = MyIntentService.ACTION_SEND_TEST_MESSAGE
-        intent.putExtra(MyIntentService.EXTRA_MESSAGE, message)
 
-        pendingIntent =
-            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent)
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_note_24)
+            .setContentTitle("Напоминание")
+            .setContentText("Пора покормить кота")
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-//        val builder = NotificationCompat.Builder(context, message)
-//        builder.setAutoCancel(true)
-//            .setDefaults(Notification.DEFAULT_ALL)
-//            .setWhen(System.currentTimeMillis())
-//            .setSmallIcon(R.mipmap.sym_def_app_icon)
-//            .setContentTitle("Note")
-//            .setDefaults(Notification.DEFAULT_LIGHTS or Notification.DEFAULT_SOUND)
-//
-//        val notificationManager =
-//            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//        notificationManager.notify(1, builder.build())
-    }
-
-    fun cancelAlarm(context: Context) {
-        pendingIntent?.let {
-            val alarmManager: AlarmManager =
-                context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            alarmManager.cancel(it)
+        with(NotificationManagerCompat.from(context)) {
+            notify(NOTIFICATION_ID, builder.build())
+            // посылаем уведомление
         }
     }
 
 
+    fun cancelAlarm(context: Context) {
+
+    }
+
+
+    companion object {
+        const val NOTIFICATION_ID = 0
+        const val CHANNEL_ID = "channelID"
+        const val MY_APP_CHANNEL_NAME = "MyApp"
+    }
 }
