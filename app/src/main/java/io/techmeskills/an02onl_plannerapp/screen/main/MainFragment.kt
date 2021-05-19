7d333b3aabbb69c995216e48b5ca0e29fd652e4c
Dzenasad <no@more>
2021-05-19 20:58:32 +0300

    Move MainFragment.kt to CoordinatorLayout

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
@@ -1,14 +1,15 @@
 package io.techmeskills.an02onl_plannerapp.screen.main
 
-import io.techmeskills.an02onl_plannerapp.model.receiver.ConnectionLiveDataReceiver
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.michaelflisar.dialogs.events.BaseDialogEvent
 import com.michaelflisar.dialogs.events.DialogInputEvent
@@ -108,6 +109,8 @@ class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_m
                     .show()
             }
         }
+
+        viewBinding.toolbarLayout
 
     }
 
